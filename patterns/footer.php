d7abd17755cc224650f152a19953e892a086e28c
Chairuddinsyah Siregar <si294r@yahoo.com>
2024-07-27 18:46:41 +0700

    Initial Commit

---
+++ patterns/footer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Title: footer
+ * Slug: sekolah-alam-cikeas/footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fixed","flexSize":"40%"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo-sac.jpg" alt=""/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Sekolah Alam adalah sekolah formal yang konsep pendidikan, lingkungan belajar dan metode pembelajarannya menggunakan alam.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:600">+62 8111 1199 24</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:600">support@sac.co.id</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"dark-blue"} -->
+<p class="has-dark-blue-color has-text-color has-link-color" style="font-style:normal;font-weight:600">Main Menu</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:template-part {"slug":"footer-main-menu"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"},"position":{"type":""},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"dark-blue"} -->
+<p class="has-dark-blue-color has-text-color has-link-color" style="font-style:normal;font-weight:600">Link Lainnya</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:template-part {"slug":"footer-link-lainnya"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|50"},"position":{"type":""},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"wrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}},"typography":{"fontStyle":"normal","fontWeight":"600"}},"textColor":"dark-blue"} -->
+<p class="has-dark-blue-color has-text-color has-link-color" style="font-style:normal;font-weight:600">Follow Us</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"30px"},"margin":{"top":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--20);padding-top:20px;padding-bottom:30px"><!-- wp:separator {"className":"is-style-default","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"very-pale-blue"} -->
+<hr class="wp-block-separator has-text-color has-very-pale-blue-color has-alpha-channel-opacity has-very-pale-blue-background-color has-background is-style-default" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"small"} -->
+<p class="has-text-align-left has-small-font-size">Copyright  2024 Sekolah Alam Cikeas.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/header.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: header
+ * Slug: sekolah-alam-cikeas/header
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"layout":{"inherit":"true","type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"bottom":"30px","top":"30px"}},"position":{"type":""}},"layout":{"type":"flex","justifyContent":"space-between","orientation":"horizontal"}} -->
+<div class="wp-block-group alignfull" style="padding-top:30px;padding-bottom:30px"><!-- wp:group {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"shouldSyncIcon":true} /-->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:image {"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/logo-sac.jpg" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:template-part {"slug":"header-menu","area":"uncategorized"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
