0ea03eadf40486e636e3578f6f7eb2472361d169
Chairuddinsyah Siregar <si294r@yahoo.com>
2024-08-15 16:59:09 +0700

    update 20240821

---
+++ patterns/reviews.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Title: reviews
+ * Slug: sekolah-alam-cikeas/reviews
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"className":"background-testimoni","layout":{"type":"constrained"}} -->
+<div class="wp-block-group background-testimoni"><!-- wp:group {"backgroundColor":"dark-blue","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-dark-blue-background-color has-background"><!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"large-38","fontFamily":"headland-one"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color has-headland-one-font-family has-large-38-font-size">Testimoni &amp; Ulasan</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"typography":{"fontStyle":"normal","fontWeight":"500"}},"textColor":"white","fontSize":"small-16","fontFamily":"poppins"} -->
+<p class="has-text-align-center has-white-color has-text-color has-link-color has-poppins-font-family has-small-16-font-size" style="font-style:normal;font-weight:500">Terbukti mendidik siswa menjadi individu yang mandiri dan patriotis.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"className":"overflow-x-hidden","style":{"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group overflow-x-hidden" style="margin-top:var(--wp--preset--spacing--60)"><!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"},"layout":{"selfStretch":"fit","flexSize":null}},"backgroundColor":"white","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 1</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 2</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 3</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 4</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 5</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"testimoni-item","style":{"border":{"radius":"8px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"var:preset|spacing|20"}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group testimoni-item has-white-background-color has-background" style="border-radius:8px;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|dark-blue"}}}},"textColor":"dark-blue","fontSize":"small"} -->
+<p class="has-dark-blue-color has-text-color has-link-color has-small-font-size" style="font-style:normal;font-weight:600">Adi Surya 6</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"#95a3b0"}}},"color":{"text":"#95a3b0"},"spacing":{"padding":{"left":"var:preset|spacing|20"}}},"fontSize":"small"} -->
+<p class="has-text-color has-link-color has-small-font-size" style="color:#95a3b0;padding-left:var(--wp--preset--spacing--20)">May 26, 2023</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":1.6}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6">Pendidikan alam di Sekolah Alam Cikeas telah mengubah pandangan saya tentang pendidikan anak.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"name":"background-testimoni-opacity"},"className":"background-testimoni-opacity","layout":{"type":"constrained"}} -->
+<div class="wp-block-group background-testimoni-opacity"></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
--- script.js
+++ script.js
@@ -4,13 +4,19 @@ document.addEventListener("DOMContentLoaded", function() {
     if (document.querySelector('.background-overlay-1') != null) {
         document.querySelector('.background-overlay-1').parentElement.style.position = 'relative';
     }
+    if (document.querySelector('.background-overlay-2') != null) {
+        document.querySelector('.background-overlay-2').parentElement.style.position = 'relative';
+    }
+    if (document.querySelector('.background-overlay-3') != null) {
+        document.querySelector('.background-overlay-3').parentElement.style.position = 'relative';
+    }
 
 });
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    if (document.querySelector('.testimoni-container') != null) {
-        document.querySelector('.testimoni-container').addEventListener("wheel", function(e) {
+    if (document.querySelector('.testimoni-item') != null) {
+        document.querySelector('.testimoni-item').parentElement.addEventListener("wheel", function(e) {
             e.preventDefault();
             // scroll horizontal
             this.scrollLeft += e.deltaY;
