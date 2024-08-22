1726be718a777f85a76a867cda87de400674dfe5
Chairuddinsyah Siregar <si294r@yahoo.com>
2024-08-22 20:19:28 +0700

    update 20240822

--- script.js
+++ script.js
@@ -24,3 +24,26 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.querySelectorAll('p[class*=blog-link]').forEach( element => {
+        element.addEventListener("click", blogLinkClick);
+    });
+    document.querySelector('p.blog-link-all').style.fontWeight = 'bold';
+
+});
+
+function blogLinkClick(e) {
+    document.querySelectorAll('p[class*=blog-link]').forEach( element => {
+        element.style.fontWeight = 'normal';
+    });
+    e.target.style.fontWeight = 'bold';
+    var className = e.target.getAttribute('class').replace('link', 'container');
+
+    document.querySelectorAll('div[class*=blog-container]').forEach( element => {
+        element.style.display = 'none';
+    });
+    document.querySelector('.' + className).style.display = 'block';
+}
